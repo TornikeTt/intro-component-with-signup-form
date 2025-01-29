86b7243284c67626bd37baf7f29ec57f9dd97037
Tornike <chargeishvilitornike@gmail.com>
2025-01-29 07:05:32 +0400

    add name, lastname, email and password validation

--- script.js
+++ script.js
@@ -1,113 +1,75 @@
 const form_container = document.querySelector(".form-container");
 const form = document.querySelector(".form-container form");
 
-const first_Name_Input = document.querySelector(".input--first-name");
-const last_Name_Input = document.querySelector(".input--last-name");
-const email_Input = document.querySelector(".input--email");
-const password_Input = document.querySelector(".input--password");
+const inputsData = {
+    firstName: {
+        input: document.querySelector(".input--first-name"),
+        validationRule: /^[a-zA-Z]+$/,
+        errorMessage:
+            "First Name must use only English letters (no symbols or numbers).",
+        empty_errorMessage: "First Name cannot be empty",
+    },
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-});
+    lastName: {
+        input: document.querySelector(".input--last-name"),
+        validationRule: /^[a-zA-Z]+$/,
+        errorMessage:
+            "Last Name must use only English letters (no symbols or numbers).",
+        empty_errorMessage: "Last Name cannot be empty",
+    },
 
-first_Name_Input.addEventListener("input", (e) => {
-    const error_correct_Icon = document.querySelector(
-        ".error-icon--first-name"
-    );
-    const errorMessage = document.querySelector(".error-message--first-name");
+    email: {
+        input: document.querySelector(".input--email"),
+        validationRule: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        errorMessage: "Looks like this is not a valid email.",
+        empty_errorMessage: "Email cannot be empty",
+    },
 
-    const regex = /^[a-zA-Z]+$/;
-    const result = regex.test(e.target.value);
+    password: {
+        input: document.querySelector(".input--password"),
+        validationRule:
+            /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]{6,20}$/,
+        errorMessage:
+            "Password must be 6-20 characters, include 1 uppercase letter, and 1 symbol.",
+        empty_errorMessage: "Password cannot be empty",
+    },
+};
 
-    if (e.target.value !== "" && result === true) {
+for (const field in inputsData) {
+    const input = inputsData[field].input;
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        validateInput(input, field);
+    });
+    input.addEventListener("input", () => validateInput(input, field));
+}
+
+function validateInput(input, field) {
+    const error_correct_Icon = document.querySelector(`.error-icon--${field}`);
+    const errorMessage = document.querySelector(`.error-message--${field}`);
+    const regex = inputsData[field].validationRule;
+    const isValid = regex.test(input.value);
+    const value = input.value;
+
+    if (value !== "" && isValid === true) {
         errorMessage.innerText = "";
 
         error_correct_Icon.style.display = "flex";
         error_correct_Icon.src = "./images/correct.png";
 
-        first_Name_Input.style.borderWidth = "2px";
-        first_Name_Input.style.borderColor = "green";
+        input.style.borderWidth = "2px";
+        input.style.borderColor = "green";
     } else {
         error_correct_Icon.style.display = "block";
         error_correct_Icon.src = "./images/icon-error.svg";
 
-        first_Name_Input.style.borderWidth = "2px";
-        first_Name_Input.style.borderColor = "hsl(0, 100%, 74%)";
+        input.style.borderWidth = "2px";
+        input.style.borderColor = "hsl(0, 100%, 74%)";
 
-        if (e.target.value === "") {
-            errorMessage.innerText = "First Name cannot be empty";
-        } else if (result === false) {
-            errorMessage.innerText =
-                "First name must use only English letters (no symbols or numbers).";
+        if (value === "") {
+            errorMessage.innerText = inputsData[field].empty_errorMessage;
+        } else {
+            errorMessage.innerText = inputsData[field].errorMessage;
         }
     }
-});
-
-last_Name_Input.addEventListener("input", (e) => {
-    const error_correct_Icon = document.querySelector(".error-icon--last-name");
-    const errorMessage = document.querySelector(".error-message--last-name");
-
-    const regex = /^[a-zA-Z]+$/;
-    const result = regex.test(e.target.value);
-
-    if (e.target.value !== "" && result === true) {
-        errorMessage.innerText = "";
-
-        error_correct_Icon.style.display = "flex";
-        error_correct_Icon.src = "./images/correct.png";
-
-        last_Name_Input.style.borderWidth = "2px";
-        last_Name_Input.style.borderColor = "green";
-    } else {
-        error_correct_Icon.style.display = "block";
-        error_correct_Icon.src = "./images/icon-error.svg";
-
-        last_Name_Input.style.borderWidth = "2px";
-        last_Name_Input.style.borderColor = "hsl(0, 100%, 74%)";
-
-        if (e.target.value === "") {
-            errorMessage.innerText = "Last Name cannot be empty";
-        } else if (result === false) {
-            errorMessage.innerText =
-                "Last name must use only English letters (no symbols or numbers).";
-        }
-    }
-});
-
-email_Input.addEventListener("input", (e) => {
-    const error_correct_Icon = document.querySelector(".error-icon--email");
-    const errorMessage = document.querySelector(".error-message--email");
-
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const result = regex.test(e.target.value);
-
-    if (e.target.value !== "" && result === true) {
-        errorMessage.innerText = "";
-
-        error_correct_Icon.style.display = "flex";
-        error_correct_Icon.src = "./images/correct.png";
-
-        email_Input.style.borderWidth = "2px";
-        email_Input.style.borderColor = "green";
-    } else {
-        error_correct_Icon.style.display = "block";
-        error_correct_Icon.src = "./images/icon-error.svg";
-
-        email_Input.style.borderWidth = "2px";
-        email_Input.style.borderColor = "hsl(0, 100%, 74%)";
-
-        if (e.target.value === "") {
-            errorMessage.innerText = "Email cannot be empty";
-        } else if (result === false) {
-            errorMessage.innerText = "Looks like this is not an eamil.";
-        }
-    }
-});
-
-/* 
-    Password check:
-    1. Minimum 6 characters.
-    2. Maximum 20 characters.
-    3. Must contain at least 1 uppercase letter.
-    4. Must include at least 1 symbol.
-*/
+}
